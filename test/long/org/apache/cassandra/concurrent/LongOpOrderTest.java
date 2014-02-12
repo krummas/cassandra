408aa3c9796f7f598b27592496c5c63d64e33889
belliottsmith <github@sub.laerad.com>
2014-02-12 00:13:53 +0000

    remove duplicate LongOpOrderTest

--- test/long/org/apache/cassandra/concurrent/LongOpOrderTest.java
+++ test/long/org/apache/cassandra/concurrent/LongOpOrderTest.java
@@ -1,33 +1,36 @@
 package org.apache.cassandra.concurrent;
 
+import org.apache.cassandra.utils.concurrent.NonBlockingQueue;
+import org.apache.cassandra.utils.concurrent.NonBlockingQueueView;
 import org.apache.cassandra.utils.concurrent.OpOrder;
 
-import org.cliffc.high_scale_lib.NonBlockingHashMap;
 import org.junit.*;
 import org.slf4j.*;
 
 import static org.junit.Assert.*;
 
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.Semaphore;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
 
-// TODO: we don't currently test SAFE functionality at all!
-// TODO: should also test markBlocking and SyncOrdered
-public class LongOpOrderTest
+public class OpOrderTest
 {
 
-    private static final Logger logger = LoggerFactory.getLogger(LongOpOrderTest.class);
+    private static final Logger logger = LoggerFactory.getLogger(OpOrderTest.class);
 
-    static final int CONSUMERS = 4;
-    static final int PRODUCERS = 32;
+    static final int CONSUMERS = Runtime.getRuntime().availableProcessors();
+    static final int PRODUCERS = CONSUMERS;
 
     static final long RUNTIME = TimeUnit.MINUTES.toMillis(5);
-    static final long REPORT_INTERVAL = TimeUnit.MINUTES.toMillis(1);
+    static final long REPORT_INTERVAL = TimeUnit.SECONDS.toMillis(10);
+    static final boolean EXPOSE_BARRIER = false;
 
     static final Thread.UncaughtExceptionHandler handler = new Thread.UncaughtExceptionHandler()
     {
@@ -39,23 +42,20 @@ public class LongOpOrderTest
         }
     };
 
-    final OpOrder order = new OpOrder();
+    final OpOrder ordering = new OpOrder();
     final AtomicInteger errors = new AtomicInteger();
 
     class TestOrdering implements Runnable
     {
 
-        final int[] waitNanos = new int[1 << 16];
-        volatile State state = new State();
         final ScheduledExecutorService sched;
+        volatile State state = new State();
+        volatile AtomicInteger running = new AtomicInteger();
 
         TestOrdering(ExecutorService exec, ScheduledExecutorService sched)
         {
             this.sched = sched;
-            final ThreadLocalRandom rnd = ThreadLocalRandom.current();
-            for (int i = 0 ; i < waitNanos.length ; i++)
-                waitNanos[i] = rnd.nextInt(5000);
-            for (int i = 0 ; i < PRODUCERS / CONSUMERS ; i++)
+            for (int i = 0 ; i < Math.max(1, PRODUCERS / CONSUMERS) ; i++)
                 exec.execute(new Producer());
             exec.execute(this);
         }
@@ -66,7 +66,8 @@ public class LongOpOrderTest
             final long until = System.currentTimeMillis() + RUNTIME;
             long lastReport = System.currentTimeMillis();
             long count = 0;
-            long opCount = 0;
+            OpOrder.Group min = null;
+            ThreadLocalRandom rnd = ThreadLocalRandom.current();
             while (true)
             {
                 long now = System.currentTimeMillis();
@@ -75,34 +76,45 @@ public class LongOpOrderTest
                 if (now > lastReport + REPORT_INTERVAL)
                 {
                     lastReport = now;
-                    logger.info(String.format("%s: Executed %d barriers with %d operations. %.0f%% complete.",
-                            Thread.currentThread().getName(), count, opCount, 100 * (1 - ((until - now) / (double) RUNTIME))));
+                    logger.info(String.format("Executed %d barriers. %.0f%% complete.",
+                            count, 100 * (1 - ((until - now) / (double) RUNTIME))));
                 }
-                try
-                {
-                    Thread.sleep(0, waitNanos[((int) (count & (waitNanos.length - 1)))]);
-                } catch (InterruptedException e)
-                {
-                    e.printStackTrace();
-                }
-
                 final State s = state;
-                s.barrier = order.newBarrier();
+                final OpOrder.Barrier barrier = ordering.newBarrier();
+                if (EXPOSE_BARRIER)
+                {
+                    s.barrier = barrier;
+                }
                 s.replacement = new State();
-                s.barrier.issue();
-                s.barrier.await();
-                s.check();
-                opCount += s.totalCount();
+                AtomicInteger unfinished = running;
+                running = new AtomicInteger();
+                barrier.issue();
+                if (rnd.nextFloat() < 0.05f)
+                {
+                    while (!barrier.allPriorOpsAreFinished());
+                }
+                else
+                {
+                    barrier.await();
+                }
                 state = s.replacement;
+                if (unfinished.get() != 0)
+                {
+                    errors.incrementAndGet();
+                    logger.error("Operations were running that should have all finished");
+                }
+                s.check(barrier, min);
+                final OpOrder.Group expectMin = min;
                 sched.schedule(new Runnable()
                 {
                     @Override
                     public void run()
                     {
-                        s.check();
+                        s.check(barrier, expectMin);
                     }
                 }, 1, TimeUnit.SECONDS);
                 count++;
+                min = barrier.getSyncPoint();
             }
         }
 
@@ -111,68 +123,61 @@ public class LongOpOrderTest
 
             volatile OpOrder.Barrier barrier;
             volatile State replacement;
-            final NonBlockingHashMap<OpOrder.Group, AtomicInteger> count = new NonBlockingHashMap<>();
-            int checkCount = -1;
+            AtomicReference<OpOrder.Group> max = new AtomicReference<>();
+            AtomicReference<OpOrder.Group> min = new AtomicReference<>();
+            AtomicInteger running = new AtomicInteger();
 
-            boolean accept(OpOrder.Group opGroup)
+            boolean accept(OpOrder.Group group)
             {
-                if (barrier != null && !barrier.isAfter(opGroup))
-                    return false;
-                AtomicInteger c;
-                if (null == (c = count.get(opGroup)))
+                if (EXPOSE_BARRIER)
                 {
-                    count.putIfAbsent(opGroup, new AtomicInteger());
-                    c = count.get(opGroup);
+                    if (barrier != null && !barrier.isAfter(group))
+                        return false;
+                    running.incrementAndGet();
+                    while (true)
+                    {
+                        OpOrder.Group curmax = max.get();
+                        if (curmax != null && curmax.compareTo(group) >= 0)
+                            break;
+                        if (max.compareAndSet(curmax, group))
+                            break;
+                    }
+                    running.decrementAndGet();
                 }
-                c.incrementAndGet();
+                while (true)
+                {
+                    OpOrder.Group curmin = min.get();
+                    if (curmin != null && curmin.compareTo(group) <= 0)
+                        break;
+                    if (min.compareAndSet(curmin, group))
+                        break;
+                }
                 return true;
             }
 
-            int totalCount()
+            void check(OpOrder.Barrier barrier, OpOrder.Group expectMin)
             {
-                int c = 0;
-                for (AtomicInteger v : count.values())
-                    c += v.intValue();
-                return c;
-            }
-
-            void check()
-            {
-                boolean delete;
-                if (checkCount >= 0)
+                if (EXPOSE_BARRIER)
                 {
-                    if (checkCount != totalCount())
+                    if (running.get() != 0)
                     {
                         errors.incrementAndGet();
-                        logger.error("Received size changed after barrier finished: {} vs {}", checkCount, totalCount());
+                        logger.error("Operations were running that should have all finished (2)");
                     }
-                    delete = true;
+                    if (max.get() != null && max.get().compareTo(barrier.getSyncPoint()) > 0)
+                    {
+                        errors.incrementAndGet();
+                        logger.error("We got an operation from the future");
+                    }
                 }
-                else
+                if (expectMin != null && min.get() != null && min.get().compareTo(expectMin) < 0)
                 {
-                    checkCount = totalCount();
-                    delete = false;
-                }
-                for (Map.Entry<OpOrder.Group, AtomicInteger> e : count.entrySet())
-                {
-                    if (e.getKey().compareTo(barrier.getSyncPoint()) > 0)
-                    {
-                        errors.incrementAndGet();
-                        logger.error("Received an operation that was created after the barrier was issued.");
-                    }
-                    if (TestOrdering.this.count.get(e.getKey()).intValue() != e.getValue().intValue())
-                    {
-                        errors.incrementAndGet();
-                        logger.error("Missing registered operations. {} vs {}", TestOrdering.this.count.get(e.getKey()).intValue(), e.getValue().intValue());
-                    }
-                    if (delete)
-                        TestOrdering.this.count.remove(e.getKey());
+                    errors.incrementAndGet();
+                    logger.error("We got an operation from the past");
                 }
             }
 
         }
-
-        final NonBlockingHashMap<OpOrder.Group, AtomicInteger> count = new NonBlockingHashMap<>();
 
         class Producer implements Runnable
         {
@@ -180,25 +185,138 @@ public class LongOpOrderTest
             {
                 while (true)
                 {
-                    AtomicInteger c;
-                    OpOrder.Group opGroup = order.start();
+                    OpOrder.Group group = ordering.start();
                     try
                     {
-                        if (null == (c = count.get(opGroup)))
-                        {
-                            count.putIfAbsent(opGroup, new AtomicInteger());
-                            c = count.get(opGroup);
-                        }
-                        c.incrementAndGet();
                         State s = state;
-                        while (!s.accept(opGroup))
+                        AtomicInteger counter = running;
+                        counter.incrementAndGet();
+                        while (!s.accept(group))
                             s = s.replacement;
+                        counter.decrementAndGet();
                     }
                     finally
                     {
-                        opGroup.finishOne();
+                        group.finishOne();
                     }
                 }
+            }
+        }
+
+    }
+
+    class TestResourceClearing
+    {
+
+        final class Resource implements Runnable
+        {
+            volatile Runnable run = this;
+            public void run() { }
+        }
+
+        final NonBlockingQueue<Resource> read = new NonBlockingQueue<>();
+        final NonBlockingQueueView<Resource> clean = read.view();
+
+        TestResourceClearing(ExecutorService exec)
+        {
+            final Semaphore raceAhead = new Semaphore(50000);
+            exec.execute(new Producer(raceAhead));
+            exec.execute(new Consumer());
+            exec.execute(new Cleaner(raceAhead));
+        }
+
+        class Producer implements Runnable
+        {
+            final Semaphore raceAhead;
+            Producer(Semaphore raceAhead)
+            {
+                this.raceAhead = raceAhead;
+            }
+
+            public void run()
+            {
+                try
+                {
+                    while (true)
+                    {
+                        for (int i = 0 ; i < 1000 ; i++)
+                            read.append(new Resource());
+                        while (true)
+                        {
+                            raceAhead.acquire(1000);
+                            for (int i = 0 ; i < 1000 ; i++)
+                            {
+                                read.advance();
+                                read.append(new Resource());
+                            }
+                        }
+                    }
+                }
+                catch (InterruptedException e)
+                {
+                    throw new IllegalStateException();
+                }
+            }
+        }
+
+        class Consumer implements Runnable
+        {
+            public void run()
+            {
+                while (true)
+                {
+                    OpOrder.Group group = ordering.start();
+                    try
+                    {
+                        for (Resource item : read)
+                            item.run.run();
+                    }
+                    finally
+                    {
+                        group.finishOne();
+                    }
+                }
+            }
+        }
+
+        class Cleaner implements Runnable
+        {
+            final Semaphore raceAhead;
+            Cleaner(Semaphore raceAhead)
+            {
+                this.raceAhead = raceAhead;
+            }
+
+            public void run()
+            {
+                List<Resource> collect = new ArrayList<>(10000);
+                while (true)
+                {
+                    int count = clean.drainTo(collect, 10000);
+                    int offset = 0;
+                    for (int i = 0 ; i < count ; i++)
+                    {
+                        if (collect.get(i) == read.peek())
+                        {
+                            collect(collect, offset, i);
+                            offset = i;
+                            while (collect.get(i) == read.peek());
+                        }
+                    }
+                    collect(collect, offset, count);
+                }
+            }
+            private void collect(List<Resource> collect, int offset, int end)
+            {
+                OpOrder.Barrier barrier = ordering.newBarrier();
+                barrier.issue();
+                barrier.await();
+                for (int i = offset ; i < end ; i++)
+                {
+                    assert collect.get(i).run != null;
+                    collect.get(i).run = null;
+                }
+                raceAhead.release(end - offset);
             }
         }
 
@@ -209,10 +327,24 @@ public class LongOpOrderTest
     {
         errors.set(0);
         Thread.setDefaultUncaughtExceptionHandler(handler);
-        final ExecutorService exec = Executors.newCachedThreadPool(new NamedThreadFactory("checker"));
+        final ExecutorService exec = Executors.newCachedThreadPool(new NamedThreadFactory("exec"));
         final ScheduledExecutorService checker = Executors.newScheduledThreadPool(1, new NamedThreadFactory("checker"));
         for (int i = 0 ; i < CONSUMERS ; i++)
             new TestOrdering(exec, checker);
+        exec.shutdown();
+        exec.awaitTermination((long) (RUNTIME * 1.1), TimeUnit.MILLISECONDS);
+        assertTrue(exec.isShutdown());
+        assertTrue(errors.get() == 0);
+    }
+
+    @Test
+    public void testResourceClearing() throws InterruptedException
+    {
+        errors.set(0);
+        Thread.setDefaultUncaughtExceptionHandler(handler);
+        final ExecutorService exec = Executors.newCachedThreadPool(new NamedThreadFactory("exec"));
+        for (int i = 0 ; i < PRODUCERS ; i++)
+            new TestResourceClearing(exec);
         exec.shutdown();
         exec.awaitTermination((long) (RUNTIME * 1.1), TimeUnit.MILLISECONDS);
         assertTrue(exec.isShutdown());
