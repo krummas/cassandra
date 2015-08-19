df6470d6070a0f6debba72ff82f3f1ba8150d589
Marcus Eriksson <marcuse@apache.org>
2015-08-19 09:37:31 +0200

    revert unrelated change

--- test/unit/org/apache/cassandra/db/lifecycle/RealTransactionsTest.java
+++ test/unit/org/apache/cassandra/db/lifecycle/RealTransactionsTest.java
@@ -132,8 +132,8 @@ public class RealTransactionsTest extends SchemaLoader
     {
         cfs.truncateBlocking();
 
-        String schema = "CREATE TABLE \"%s\".\"%s\" (key ascii, name ascii, val ascii, val1 ascii, PRIMARY KEY (key, name))";
-        String query = "INSERT INTO \"%s\".\"%s\" (key, name, val) VALUES (?, ?, ?)";
+        String schema = "CREATE TABLE %s.%s (key ascii, name ascii, val ascii, val1 ascii, PRIMARY KEY (key, name))";
+        String query = "INSERT INTO %s.%s (key, name, val) VALUES (?, ?, ?)";
 
         try (CQLSSTableWriter writer = CQLSSTableWriter.builder()
                                                        .inDirectory(cfs.getDirectories().getDirectoryForNewSSTables())
