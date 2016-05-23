a56b598db9a1c545282d5f46b90bcf24a1648954
Marcus Eriksson <marcuse@apache.org>
2016-05-23 10:16:33 +0200

    minor purge test

--- test/unit/org/apache/cassandra/db/compaction/NeverPurgeTest.java
+++ test/unit/org/apache/cassandra/db/compaction/NeverPurgeTest.java
@@ -32,6 +32,7 @@ import org.apache.cassandra.db.columniterator.OnDiskAtomIterator;
 import org.apache.cassandra.io.sstable.ISSTableScanner;
 import org.apache.cassandra.io.sstable.SSTableReader;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class NeverPurgeTest extends CQLTester
@@ -59,6 +60,31 @@ public class NeverPurgeTest extends CQLTester
         testHelper("DELETE FROM %s WHERE a=1");
     }
 
+    @Test
+    public void minorNeverPurgeTombstonesTest() throws Throwable
+    {
+        createTable("CREATE TABLE %s (a int, b int, c text, PRIMARY KEY (a, b)) WITH gc_grace_seconds = 0");
+        ColumnFamilyStore cfs = Keyspace.open(keyspace()).getColumnFamilyStore(currentTable());
+        cfs.disableAutoCompaction();
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 1000; j++)
+            {
+                execute("INSERT INTO %s (a, b, c) VALUES (" + j + ", 2, '3')");
+            }
+            cfs.forceBlockingFlush();
+        }
+
+        execute("UPDATE %s SET c = null WHERE a=1 AND b=2");
+        execute("DELETE FROM %s WHERE a=2 AND b=2");
+        execute("DELETE FROM %s WHERE a=3");
+        cfs.forceBlockingFlush();
+        cfs.enableAutoCompaction();
+        while (cfs.getSSTables().size() > 1)
+            Thread.sleep(100);
+        verifyContainsTombstones(cfs.getSSTables(), 3);
+    }
+
     private void testHelper(String deletionStatement) throws Throwable
     {
         createTable("CREATE TABLE %s (a int, b int, c text, PRIMARY KEY (a, b)) WITH gc_grace_seconds = 0");
@@ -68,14 +94,14 @@ public class NeverPurgeTest extends CQLTester
         Thread.sleep(1000);
         cfs.forceBlockingFlush();
         cfs.forceMajorCompaction();
-        verifyContainsTombstones(cfs.getSSTables());
+        verifyContainsTombstones(cfs.getSSTables(), 1);
     }
 
-    private void verifyContainsTombstones(Collection<SSTableReader> sstables) throws Exception
+    private void verifyContainsTombstones(Collection<SSTableReader> sstables, int expectedTombstoneCount) throws Exception
     {
         assertTrue(sstables.size() == 1); // always run a major compaction before calling this
         SSTableReader sstable = sstables.iterator().next();
-        boolean hasTombstones = false;
+        int tombstoneCount = 0;
         try (ISSTableScanner scanner = sstable.getScanner())
         {
             while (scanner.hasNext())
@@ -83,17 +109,17 @@ public class NeverPurgeTest extends CQLTester
                 try (OnDiskAtomIterator iter = scanner.next())
                 {
                     if (iter.getColumnFamily().deletionInfo().getTopLevelDeletion().localDeletionTime < Integer.MAX_VALUE)
-                        hasTombstones = true;
+                        tombstoneCount++;
 
                     while (iter.hasNext())
                     {
                         OnDiskAtom atom = iter.next();
                         if (atom instanceof DeletedCell || atom instanceof RangeTombstone)
-                            hasTombstones = true;
+                            tombstoneCount++;
                     }
                 }
             }
         }
-        assertTrue(hasTombstones);
+        assertEquals(tombstoneCount, expectedTombstoneCount);
     }
 }
