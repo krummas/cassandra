5aff616fb5e01846d9b28bdb8402fc5ce50db682
Marcus Eriksson <marcuse@apache.org>
2018-01-29 15:30:17 +0100

    handle throwable during startup

--- src/java/org/apache/cassandra/io/sstable/format/SSTableReader.java
+++ src/java/org/apache/cassandra/io/sstable/format/SSTableReader.java
@@ -453,9 +453,9 @@ public abstract class SSTableReader extends SSTable implements SelfRefCounted<SS
         {
             sstableMetadata = descriptor.getMetadataSerializer().deserialize(descriptor, types);
         }
-        catch (IOException e)
+        catch (Throwable t)
         {
-            throw new CorruptSSTableException(e, descriptor.filenameFor(Component.STATS));
+            throw new CorruptSSTableException(t, descriptor.filenameFor(Component.STATS));
         }
         ValidationMetadata validationMetadata = (ValidationMetadata) sstableMetadata.get(MetadataType.VALIDATION);
         StatsMetadata statsMetadata = (StatsMetadata) sstableMetadata.get(MetadataType.STATS);
@@ -506,7 +506,7 @@ public abstract class SSTableReader extends SSTable implements SelfRefCounted<SS
         catch (Throwable t)
         {
             sstable.selfRef().release();
-            throw t;
+            throw new CorruptSSTableException(t, descriptor.filenameFor(Component.DATA));
         }
     }
 
