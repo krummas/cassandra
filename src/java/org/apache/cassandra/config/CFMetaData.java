3d49be9ec123ce843c3f80478fa44e93b586c6d0
Marcus Eriksson <marcuse@apache.org>
2020-06-29 16:12:09 +0200

    Make sure we can't select or write the hidden columns

--- src/java/org/apache/cassandra/config/CFMetaData.java
+++ src/java/org/apache/cassandra/config/CFMetaData.java
@@ -1024,7 +1024,7 @@ public final class CFMetaData
     public ColumnDefinition getColumnDefinitionForCQL(ByteBuffer name)
     {
         ColumnDefinition cd = getColumnDefinition(name);
-        return hiddenColumns.contains(cd) ? null : cd;
+        return hiddenColumns.contains(cd) || cd.isHidden() ? null : cd;
     }
 
     public static boolean isNameValid(String name)
