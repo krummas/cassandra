4fbf9fc062e2394d375a2dc02224abf32923f0ba
Marcus Eriksson <marcuse@apache.org>
2021-04-08 08:29:15 +0200

    move to test/unit

--- test/unit/org/apache/cassandra/JStackJUnitTask.java
+++ test/unit/org/apache/cassandra/JStackJUnitTask.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.cassandra.jstackjunit;
+package org.apache.cassandra;
 
 import java.io.BufferedReader;
 import java.io.IOException;
