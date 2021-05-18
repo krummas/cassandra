56d394d8a847ae9e0f9cdc0ebef67bae2b51e60b
Marcus Eriksson <marcuse@apache.org>
2021-05-18 14:10:23 +0200

    allow optimised streams with force repair
    make sure preview repairs don't optimise streams unless configured to (isIncremental==true for these)

--- src/java/org/apache/cassandra/service/StorageService.java
+++ src/java/org/apache/cassandra/service/StorageService.java
@@ -5969,7 +5969,7 @@ public class StorageService extends NotificationBroadcasterSupport implements IE
                                                                         e -> e.getValue().stream().map(InetAddressAndPort::toString).collect(Collectors.toSet())));
     }
 
-    public boolean autoOptimiseIncRepairStreams()
+    public boolean getAutoOptimiseIncRepairStreams()
     {
         return DatabaseDescriptor.autoOptimiseIncRepairStreams();
     }
@@ -5979,7 +5979,7 @@ public class StorageService extends NotificationBroadcasterSupport implements IE
         DatabaseDescriptor.setAutoOptimiseIncRepairStreams(enabled);
     }
 
-    public boolean autoOptimiseFullRepairStreams()
+    public boolean getAutoOptimiseFullRepairStreams()
     {
         return DatabaseDescriptor.autoOptimiseFullRepairStreams();
     }
@@ -5989,7 +5989,7 @@ public class StorageService extends NotificationBroadcasterSupport implements IE
         DatabaseDescriptor.setAutoOptimiseFullRepairStreams(enabled);
     }
 
-    public boolean autoOptimisePreviewRepairStreams()
+    public boolean getAutoOptimisePreviewRepairStreams()
     {
         return DatabaseDescriptor.autoOptimisePreviewRepairStreams();
     }
