a4e24c420e671ee2b202ce28f3de650a2fb9f65d
Marcus Eriksson <marcuse@apache.org>
2022-02-07 10:21:21 +0100

    small cleanups

--- src/java/org/apache/cassandra/repair/consistent/RepairedState.java
+++ src/java/org/apache/cassandra/repair/consistent/RepairedState.java
@@ -23,23 +23,16 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashSet;
-import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 
 import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.Iterables;
-import com.google.common.collect.Iterators;
-import com.google.common.collect.PeekingIterator;
 import com.google.common.collect.Sets;
 
 import org.apache.cassandra.dht.Range;
 import org.apache.cassandra.dht.Token;
-import org.apache.cassandra.service.ActiveRepairService;
-import org.apache.cassandra.utils.UUIDGen;
 
 import static org.apache.cassandra.service.ActiveRepairService.UNREPAIRED_SSTABLE;
 
@@ -207,34 +200,20 @@ public class RepairedState
         return state;
     }
 
-    private static List<Section> levelsToSections(List<Level> levels)
+    public synchronized void add(Collection<Range<Token>> ranges, long repairedAt)
     {
-        List<Section> sections = new ArrayList<>();
-        for (Level level : levels)
-        {
-            for (Range<Token> range : level.ranges)
-            {
-                sections.add(new Section(range, level.repairedAt));
-            }
-        }
-        sections.sort(Section.tokenComparator);
-        return sections;
+        addAll(Collections.singletonList(new Level(ranges, repairedAt)));
     }
-    public void add( List<Level> newLevels)
+
+    public void addAll(List<Level> newLevels)
     {
         State lastState = state;
+        List<Level> tmp = new ArrayList<>(lastState.levels.size() + newLevels.size());
+        tmp.addAll(lastState.levels);
+        tmp.addAll(newLevels);
+        tmp.sort(Level.timeComparator);
 
-        List<Level> levels = new ArrayList<>(lastState.levels.size() + newLevels.size());
-        levels.addAll(lastState.levels);
-        levels.addAll(newLevels);
-        levels.sort(Level.timeComparator);
-
-        processLevels(levels);
-    }
-    
-	private void processLevels(List<Level> tmp) {
-
-        List<Level> levels = new ArrayList<>(tmp.size() );
+        List<Level> levels = new ArrayList<>(tmp.size());
         List<Range<Token>> covered = new ArrayList<>();
 
         for (Level level : tmp)
@@ -258,18 +237,8 @@ public class RepairedState
             }
         }
         sections.sort(Section.tokenComparator);
-
         state = new State(levels, covered, sections);
-        
 	}
-    public synchronized void add(Collection<Range<Token>> ranges, long repairedAt)
-    {
-        Level newLevel = new Level(ranges, repairedAt);
-        List<Level> levels = new LinkedList<Level>();
-        levels.add(newLevel);
-        add(levels);
- 
-     }
 
     public long minRepairedAt(Collection<Range<Token>> ranges)
     {
--- test/unit/org/apache/cassandra/repair/consistent/BulkRepairStateTest.java
+++ test/unit/org/apache/cassandra/repair/consistent/BulkRepairStateTest.java
@@ -19,145 +19,144 @@ package org.apache.cassandra.repair.consistent;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.LinkedList;
 import java.util.List;
 
 import org.apache.cassandra.dht.Murmur3Partitioner;
 import org.apache.cassandra.dht.Range;
 import org.apache.cassandra.dht.Token;
-import org.junit.Assert;
+
 import org.junit.Test;
 
 import com.google.common.collect.Lists;
 
-public class BulkRepairStateTest {
-	   private static Token tk(long t)
-	    {
-	        return new Murmur3Partitioner.LongToken(t);
-	    }
+import static org.junit.Assert.assertEquals;
 
-	    private static Range<Token> range(long left, long right)
-	    {
-	        return new Range<>(tk(left), tk(right));
-	    }
+public class BulkRepairStateTest
+{
+    private static Token tk(long t)
+    {
+        return new Murmur3Partitioner.LongToken(t);
+    }
 
-	    private static List<Range<Token>> ranges(long... tokens)
-	    {
-	        assert tokens.length %2 == 0;
-	        List<Range<Token>> ranges = new ArrayList<>();
-	        for (int i=0; i<tokens.length; i+=2)
-	        {
-	            ranges.add(range(tokens[i], tokens[i+1]));
+    private static Range<Token> range(long left, long right)
+    {
+        return new Range<>(tk(left), tk(right));
+    }
 
-	        }
-	        return ranges;
-	    }
+    private static List<Range<Token>> ranges(long... tokens)
+    {
+        assert tokens.length % 2 == 0;
+        List<Range<Token>> ranges = new ArrayList<>();
+        for (int i = 0; i < tokens.length; i += 2)
+        {
+            ranges.add(range(tokens[i], tokens[i + 1]));
+        }
+        return ranges;
+    }
 
-	    private static RepairedState.Level level(Collection<Range<Token>> ranges, long repairedAt)
-	    {
-	        return new RepairedState.Level(ranges, repairedAt);
-	    }
+    private static RepairedState.Level level(Collection<Range<Token>> ranges, long repairedAt)
+    {
+        return new RepairedState.Level(ranges, repairedAt);
+    }
 
-	    private static RepairedState.Section sect(Range<Token> range, long repairedAt)
-	    {
-	        return new RepairedState.Section(range, repairedAt);
-	    }
+    private static RepairedState.Section sect(Range<Token> range, long repairedAt)
+    {
+        return new RepairedState.Section(range, repairedAt);
+    }
 
-	    private static RepairedState.Section sect(int l, int r, long time)
-	    {
-	        return sect(range(l, r), time);
-	    }
+    private static RepairedState.Section sect(int l, int r, long time)
+    {
+        return sect(range(l, r), time);
+    }
 
-	    private static <T> List<T> l(T... contents)
-	    {
-	        return Lists.newArrayList(contents);
-	    }
+    private static <T> List<T> l(T... contents)
+    {
+        return Lists.newArrayList(contents);
+    }
 
-	    @Test
-	    public void mergeOverlapping()
-	    {
-	        RepairedState repairs = new RepairedState();
-	        List<RepairedState.Level> list = new LinkedList<RepairedState.Level>();
-            list.add( new RepairedState.Level(ranges(100, 300), 5 ));
-            list.add( new RepairedState.Level(ranges(200, 400), 6 ));
-	        repairs.add(list);
+    @Test
+    public void mergeOverlapping()
+    {
+        RepairedState repairs = new RepairedState();
+        List<RepairedState.Level> list = new ArrayList<>();
+        list.add(new RepairedState.Level(ranges(100, 300), 5));
+        list.add(new RepairedState.Level(ranges(200, 400), 6));
+        repairs.addAll(list);
 
-	        RepairedState.State state = repairs.state();
-	        Assert.assertEquals(l(level(ranges(200, 400), 6), level(ranges(100, 200), 5)), state.levels);
-	        Assert.assertEquals(l(sect(range(100, 200), 5), sect(range(200, 400), 6)), state.sections);
-	        Assert.assertEquals(ranges(100, 400), state.covered);
-	    }
-	    @Test
-	    public void mergeSameRange()
-	    {
-	        RepairedState repairs = new RepairedState();
-            List<RepairedState.Level> list = new LinkedList<RepairedState.Level>();
-            list.add( new RepairedState.Level(ranges(100, 400), 5 ));
-            list.add( new RepairedState.Level(ranges(100, 400), 6 ));
-            repairs.add(list);
+        RepairedState.State state = repairs.state();
+        assertEquals(l(level(ranges(200, 400), 6), level(ranges(100, 200), 5)), state.levels);
+        assertEquals(l(sect(range(100, 200), 5), sect(range(200, 400), 6)), state.sections);
+        assertEquals(ranges(100, 400), state.covered);
+    }
 
-	        RepairedState.State state = repairs.state();
-	        Assert.assertEquals(l(level(ranges(100, 400), 6)), state.levels);
-	        Assert.assertEquals(l(sect(range(100, 400), 6)), state.sections);
-	        Assert.assertEquals(ranges(100, 400), state.covered);
-	    }
+    @Test
+    public void mergeSameRange()
+    {
+        RepairedState repairs = new RepairedState();
+        List<RepairedState.Level> list = new ArrayList<>();
+        list.add(new RepairedState.Level(ranges(100, 400), 5));
+        list.add(new RepairedState.Level(ranges(100, 400), 6));
+        repairs.addAll(list);
 
-	    @Test
-	    public void mergeLargeRange()
-	    {
-	        RepairedState repairs = new RepairedState();
+        RepairedState.State state = repairs.state();
+        assertEquals(l(level(ranges(100, 400), 6)), state.levels);
+        assertEquals(l(sect(range(100, 400), 6)), state.sections);
+        assertEquals(ranges(100, 400), state.covered);
+    }
 
-            List<RepairedState.Level> list = new LinkedList<RepairedState.Level>();
-            list.add( new RepairedState.Level(ranges(200, 300), 5 ));
-            list.add( new RepairedState.Level(ranges(100, 400), 6 ));
-            repairs.add(list);
+    @Test
+    public void mergeLargeRange()
+    {
+        RepairedState repairs = new RepairedState();
 
-	        RepairedState.State state = repairs.state();
-	        Assert.assertEquals(l(level(ranges(100, 400), 6)), state.levels);
-	        Assert.assertEquals(l(sect(range(100, 400), 6)), state.sections);
-	        Assert.assertEquals(ranges(100, 400), state.covered);
-	    }
+        List<RepairedState.Level> list = new ArrayList<>();
+        list.add(new RepairedState.Level(ranges(200, 300), 5));
+        list.add(new RepairedState.Level(ranges(100, 400), 6));
+        repairs.addAll(list);
 
-	    @Test
-	    public void mergeSmallRange()
-	    {
-	        RepairedState repairs = new RepairedState();
+        RepairedState.State state = repairs.state();
+        assertEquals(l(level(ranges(100, 400), 6)), state.levels);
+        assertEquals(l(sect(range(100, 400), 6)), state.sections);
+        assertEquals(ranges(100, 400), state.covered);
+    }
 
-	        List<RepairedState.Level> list = new LinkedList<RepairedState.Level>();
-	        list.add( new RepairedState.Level(ranges(100, 400), 5 ));
-            list.add( new RepairedState.Level(ranges(200, 300), 6 ));
-            repairs.add(list);
+    @Test
+    public void mergeSmallRange()
+    {
+        RepairedState repairs = new RepairedState();
 
-	        RepairedState.State state = repairs.state();
-	        Assert.assertEquals(l(level(ranges(200, 300), 6), level(ranges(100, 200, 300, 400), 5)), state.levels);
-	        Assert.assertEquals(l(sect(range(100, 200), 5), sect(range(200, 300), 6), sect(range(300, 400), 5)), state.sections);
-	        Assert.assertEquals(ranges(100, 400), state.covered);
-	    }
+        List<RepairedState.Level> list = new ArrayList<>();
+        list.add(new RepairedState.Level(ranges(100, 400), 5));
+        list.add(new RepairedState.Level(ranges(200, 300), 6));
+        repairs.addAll(list);
 
+        RepairedState.State state = repairs.state();
+        assertEquals(l(level(ranges(200, 300), 6), level(ranges(100, 200, 300, 400), 5)), state.levels);
+        assertEquals(l(sect(range(100, 200), 5), sect(range(200, 300), 6), sect(range(300, 400), 5)), state.sections);
+        assertEquals(ranges(100, 400), state.covered);
+    }
 
-	    @Test
-	    public void repairedAt()
-	    {
-	        RepairedState rs;
+    @Test
+    public void repairedAt()
+    {
+        RepairedState rs;
 
-	        // overlapping
-	        rs = new RepairedState();
-            List<RepairedState.Level> list = new LinkedList<RepairedState.Level>();
-            list.add( new RepairedState.Level(ranges(100, 300), 5 ));
-            list.add( new RepairedState.Level(ranges(200, 400), 6 ));
-            rs.add(list);
+        // overlapping
+        rs = new RepairedState();
+        List<RepairedState.Level> list = new ArrayList<>();
+        list.add(new RepairedState.Level(ranges(100, 300), 5));
+        list.add(new RepairedState.Level(ranges(200, 400), 6));
+        rs.addAll(list);
 
-	        Assert.assertEquals(5, rs.minRepairedAt(ranges(150, 250)));
-	        Assert.assertEquals(5, rs.minRepairedAt(ranges(150, 160)));
-	        Assert.assertEquals(5, rs.minRepairedAt(ranges(100, 200)));
-	        Assert.assertEquals(6, rs.minRepairedAt(ranges(200, 400)));
-	        Assert.assertEquals(0, rs.minRepairedAt(ranges(200, 401)));
-	        Assert.assertEquals(0, rs.minRepairedAt(ranges(99, 200)));
-	        Assert.assertEquals(0, rs.minRepairedAt(ranges(50, 450)));
-	        Assert.assertEquals(0, rs.minRepairedAt(ranges(50, 60)));
-	        Assert.assertEquals(0, rs.minRepairedAt(ranges(450, 460)));
-	    }
-
-
+        assertEquals(5, rs.minRepairedAt(ranges(150, 250)));
+        assertEquals(5, rs.minRepairedAt(ranges(150, 160)));
+        assertEquals(5, rs.minRepairedAt(ranges(100, 200)));
+        assertEquals(6, rs.minRepairedAt(ranges(200, 400)));
+        assertEquals(0, rs.minRepairedAt(ranges(200, 401)));
+        assertEquals(0, rs.minRepairedAt(ranges(99, 200)));
+        assertEquals(0, rs.minRepairedAt(ranges(50, 450)));
+        assertEquals(0, rs.minRepairedAt(ranges(50, 60)));
+        assertEquals(0, rs.minRepairedAt(ranges(450, 460)));
+    }
 }
 
