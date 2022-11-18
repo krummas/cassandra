cdf351261e24ad0916b9fe96020c5827612a2226
Marcus Eriksson <marcuse@apache.org>
2022-11-18 06:58:44 +0100

    fix test

--- test/unit/org/apache/cassandra/service/StorageServiceTest.java
+++ test/unit/org/apache/cassandra/service/StorageServiceTest.java
@@ -24,9 +24,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 
 import org.apache.cassandra.concurrent.ScheduledExecutors;
 import org.apache.cassandra.config.DatabaseDescriptor;
@@ -54,9 +52,6 @@ import static org.junit.Assert.fail;
 
 public class StorageServiceTest
 {
-    @Rule
-    public ExpectedException thrown = ExpectedException.none();
-
     static InetAddressAndPort aAddress;
     static InetAddressAndPort bAddress;
     static InetAddressAndPort cAddress;
@@ -303,9 +298,7 @@ public class StorageServiceTest
                 storageService.setBatchSizeWarnThresholdInKiB(2 * 1024 * 1024);
                 fail("Should have received an IllegalArgumentException batch_size_warn_threshold = 2GiB");
             }
-            catch (IllegalArgumentException ignored)
-            {
-            }
+            catch (IllegalArgumentException ignored) {}
             Assert.assertEquals(1024, storageService.getBatchSizeWarnThresholdInKiB());
         }
         finally
@@ -318,8 +311,14 @@ public class StorageServiceTest
     public void testLocalDatacenterNodesExcludedDuringRebuild()
     {
         StorageService service = StorageService.instance;
-        service.rebuild(DatabaseDescriptor.getLocalDataCenter(), "StorageServiceTest", null, null, true);
-        thrown.expect(IllegalArgumentException.class);
-        thrown.expectMessage("Cannot set source data center to be local data center, when excludeLocalDataCenter flag is set");
+        try
+        {
+            service.rebuild(DatabaseDescriptor.getLocalDataCenter(), "StorageServiceTest", null, null, true);
+            fail();
+        }
+        catch (IllegalArgumentException e)
+        {
+            Assert.assertEquals("Cannot set source data center to be local data center, when excludeLocalDataCenter flag is set", e.getMessage());
+        }
     }
 }
