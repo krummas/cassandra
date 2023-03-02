ee17642571f40891dfa7b6f058a240774a47ce0a
Sam Tunnicliffe <samt@apache.org>
2023-03-02 11:29:07 +0000

    [CEP-21] Initial transforms for modifying ownership
    Introduces transformations to modify ClusterMetadata with the affect of
    modifying data ownership and placement i.e. join, replace, move &
    decommission. These operations do not simply modify metadata however,
    so simple atomic updates to cluster metadata are not sufficient.
    Streaming data in and out of nodes must also occur and obviously read
    and write operations must continue whilst this is in progress. These
    operations then are performed in phases, planned in advance and properly
    linearized using the global log. e.g. to join an new node, the full set
    of phased range movements required is calculated, generating an
    actionable plan which is then serialised into ClusterMetadata.
    Concurrent operations are permitted as long as they only affect disjoint
    token ranges, ensuring that concurrent range movements remain safe and
    cluster invariants are preserved at all times, including in the case of
    the failure to complete any operation (i.e. failed bootstrap).
    
    This commit only adds the transformations and supporting TCM components
    (LockedRanges, InProgressSequences etc), the implementation of actually
    performing the operations follows in subsequent commits.
    
    Co-authored-by: Marcus Eriksson <marcuse@apache.org>
    Co-authored-by: Alex Petrov <oleksandr.petrov@gmail.com>
    Co-authored-by: Sam Tunnicliffe <samt@apache.org>

--- src/java/org/apache/cassandra/service/StorageService.java
+++ src/java/org/apache/cassandra/service/StorageService.java
@@ -2145,7 +2145,7 @@ public class StorageService extends NotificationBroadcasterSupport implements IE
     /**
      * All MVs have been created during bootstrap, so mark them as built
      */
-    private void markViewsAsBuilt() {
+    public void markViewsAsBuilt() {
         for (String keyspace : Schema.instance.getUserKeyspaces().names())
         {
             for (ViewMetadata view: Schema.instance.getKeyspaceMetadata(keyspace).views)
@@ -4707,7 +4707,7 @@ public class StorageService extends NotificationBroadcasterSupport implements IE
         }
     }
 
-    private void repairPaxosForTopologyChange(String reason)
+    public void repairPaxosForTopologyChange(String reason)
     {
         if (getSkipPaxosRepairOnTopologyChange() || !Paxos.useV2())
         {
@@ -5273,7 +5273,7 @@ public class StorageService extends NotificationBroadcasterSupport implements IE
         onFinish.run();
     }
 
-    private Future streamHints()
+    public Future streamHints()
     {
         return HintsService.instance.transferHints(this::getPreferredHintsStreamTarget);
     }
@@ -6129,7 +6129,7 @@ public class StorageService extends NotificationBroadcasterSupport implements IE
      * @param rangesToStreamByKeyspace keyspaces and data ranges with endpoints included for each
      * @return async Future for whether stream was success
      */
-    private Future<StreamState> streamRanges(Map<String, EndpointsByReplica> rangesToStreamByKeyspace)
+    public Future<StreamState> streamRanges(Map<String, EndpointsByReplica> rangesToStreamByKeyspace)
     {
         // First, we build a list of ranges to stream to each host, per table
         Map<String, RangesByEndpoint> sessionsToStreamByKeyspace = new HashMap<>();
